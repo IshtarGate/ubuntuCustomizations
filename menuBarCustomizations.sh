eade07def643f15df431c489be1e052c4d72c4fa
James Hounshell <jameshounshell@gmail.com>
2016-10-15 15:54:32 -0400

    added menu bar customizations script

---
+++ menuBarCustomizations.sh
@@ -0,0 +1,10 @@
+# add a cool indicator of cpu/memory/network usage
+sudo apt-get install indicator-multiload
+# launch "system load indicator" from start menu
+
+# show recent system notifications in a que in the menu bar
+sudo add-apt-repository ppa:jconti/recent-notifications
+sudo apt-get update
+sudo apt-get install indicator-notifications
+# log out and lon in to star
+
